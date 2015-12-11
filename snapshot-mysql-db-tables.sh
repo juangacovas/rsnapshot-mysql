e447ec1ad88bef6b50290c6cd123e1d25b676cc1
Juanga Covas <juangacovasegea@gmail.com>
2015-12-11 22:05:24 +0000

    - Create a restore BASH script for each database
    git-svn-id: https://62.117.221.7/svn/rsnapshot-mysql/trunk@5 7544ec68-d604-754e-9b34-880144e6ea8b

--- snapshot-mysql-db-tables.sh
+++ snapshot-mysql-db-tables.sh
@@ -32,6 +32,14 @@ TEST_RUN=$3
 
 MYSQL_USER="remotebackup"
 MYSQL_PASSWORD_FILE="/root/snapshot-db-pwd.txt"
+if [ ! -f $MYSQL_PASSWORD_FILE ] ;then 
+	echo "Cannot read: $MYSQL_PASSWORD_FILE"
+	exit 1
+fi
+if [ ! -s $MYSQL_PASSWORD_FILE ] ;then
+	echo "File is empty: $MYSQL_PASSWORD_FILE"
+	exit 1
+fi
 MYSQL_PASSWORD=`cat $MYSQL_PASSWORD_FILE`
 MYSQL_HUP="--host=$MYSQL_HOST --user=$MYSQL_USER -p$MYSQL_PASSWORD"
 MYSQL_DUMP_FLAGS="--compress --hex-blob --force --single-transaction --skip-dump-date"
@@ -74,7 +82,7 @@ fi
 echo " "
 echo "MySQL version info"
 if [ -z "$TEST_RUN" ]; then
-	mysql $MYSQL_HUP --skip-column-names -e"SHOW VARIABLES LIKE '%version%';" > $BACKUPDIR/mysql-version-$MYSQL_HOST.txt
+	mysql $MYSQL_HUP --skip-column-names -e"SHOW VARIABLES LIKE '%version%';" > $BACKUP_DIR/mysql-version-$MYSQL_HOST.txt
 fi
 mysql $MYSQL_HUP --skip-column-names -e"SHOW VARIABLES LIKE '%version%';"
 echo " "
@@ -88,6 +96,7 @@ fi
 echo " "
 
 echo "Getting database list to dump ... "
+echo " "
 
 databaselist=`mysql $MYSQL_HUP --no-auto-rehash -e "SHOW DATABASES;" | grep -Ev "(^mysql$|^phpmyadmin$|^Database$|information_schema|performance_schema|^test_|_test$|_bak$|^bak_)"`
 
@@ -113,6 +122,92 @@ for db in $databaselist; do
 	if [ -z "$TEST_RUN" ]; then
 		echo $db_table_list > $BACKUP_DIR/$db/$db-tablelist.txt
 	fi
+
+	restore_file="$BACKUP_DIR/$db/restore-$db.sh"
+	dbr="${db}_restored"
+	
+	echo "#!/bin/bash
+
+	#########################################################################
+	#
+	#	Database Restore script for backup:
+	#
+	#	from host: $MYSQL_HOST
+	#		   db: $db
+	#		 date: $TIMESTAMP
+	#
+	#########################################################################
+
+
+	RESTOREDB=\"$dbr\"
+
+	# checks
+	
+    if [ \"\$BASH\" != \"/bin/bash\" ] ;then
+            echo \"Please execute the script using /bin/bash\"
+            exit 1;
+    fi
+	if [ -z \"\$1\" ] ;then
+		echo \"Expecting target mysql host as parameter\"
+		exit 1
+	fi
+    if ls $db*.sql* 1> /dev/null 2>&1; then
+            echo \"File check OK\"
+    else
+            echo \"Cannot find $db.sql* files. Please execute the script under its directory.\"
+            exit 1
+    fi
+
+	echo \" \"
+	echo \"Database Restore script for backup:\"	
+	echo \" \"
+	echo \"      from host: $MYSQL_HOST\"
+	echo \"             db: $db\"
+	echo \"           date: $TIMESTAMP\"
+	echo \" \"
+	echo \"RESTORE TO HOST: \$1\"
+	echo \"  RESTORE TO DB: \$RESTOREDB\"
+	echo \" \"
+	
+	MYSQL_HOST=\$1
+	MYSQL_USER=\"remotebackup\"
+	MYSQL_PASSWORD_FILE=\"/root/snapshot-db-pwd.txt\"
+	if [ ! -f \$MYSQL_PASSWORD_FILE ] ;then 
+		echo \"Cannot read: \$MYSQL_PASSWORD_FILE\"
+		exit 1
+	fi
+	if [ ! -s \$MYSQL_PASSWORD_FILE ] ;then
+		echo \"File is empty: \$MYSQL_PASSWORD_FILE\"
+		exit 1
+	fi
+	MYSQL_PASSWORD=\`cat \$MYSQL_PASSWORD_FILE\`
+	MYSQL_HUP=\"--host=\$MYSQL_HOST --user=\$MYSQL_USER -p\$MYSQL_PASSWORD\"
+	MYSQL_HUP_PRINT=\"--host=\$MYSQL_HOST --user=\$MYSQL_USER -p...\"
+
+	echo \"Checking host \$1 ...\"
+	RESULT=\`mysqlshow \$MYSQL_HUP | grep -v Wildcard | grep -o Databases\`
+	if [ \"\$RESULT\" != \"Databases\" ]; then
+		echo \"ERROR: Cannot connect to mysql server using: \$MYSQL_HUP_PRINT\"
+		exit 1
+	fi
+	echo \"Mysql connection OK.\"
+
+	echo \"mysql> CREATE DATABASE IF NOT EXISTS \$RESTOREDB;\"
+	mysql \$MYSQL_HUP -e\"CREATE DATABASE IF NOT EXISTS \$RESTOREDB;\"
+
+	RESULT=\`mysqlshow \$MYSQL_HUP | grep -v Wildcard | grep -o \$RESTOREDB\`
+	if [ \"\$RESULT\" != \"\$RESTOREDB\" ]; then
+		echo \"Could connect, but could NOT create database: \$RESTOREDB using user: \$MYSQL_USER\"
+		exit 1
+	fi
+	echo \"Database \$RESTOREDB seems there.\"
+	echo \" \"
+
+	echo \"READY TO GO ... DUMP TO DB: \$RESTOREDB \$MYSQL_HUP_PRINT\"
+	echo \" \"
+	read -p \"Press [Enter] to confirm dumping ALL .sql* files\"
+	echo \" \"
+	" >$restore_file
 
 	for DBTBNG in $db_table_engine_list; do
 
@@ -144,20 +239,37 @@ for db in $databaselist; do
 		fi
 
 		if [ -z "$TEST_RUN" ]; then
+		
 			if [ $COMPRESSION == "gz" ] ;then
 				printf '.gz ... '
-				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | gzip -9 > $BACKUP_DIR/$db/$db-$table.sql.gz
+				filedump="$BACKUP_DIR/$db/$db-$table.sql.gz"
+				restorefiledump=$(basename $filedump)
+				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | gzip -9 > $filedump
+				echo "
+				echo \"Dumping $restorefiledump ...\"
+				zcat $restorefiledump | mysql \$MYSQL_HUP --default-character-set=utf8 \$RESTOREDB" >>$restore_file
 			fi
 			
 			if [ $COMPRESSION == "bz2" ] ;then
 				printf '.bz2 ... '
-				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | bzip2 -cq9 > $BACKUP_DIR/$db/$db-$table.sql.bz2
+				filedump="$BACKUP_DIR/$db/$db-$table.sql.gz"
+				restorefiledump=$(basename $filedump)
+				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | bzip2 -cq9 > $filedump
+				echo "
+				echo \"Dumping $restorefiledump ...\"
+				bunzip2 < $restorefiledump | mysql \$MYSQL_HUP --default-character-set=utf8 \$RESTOREDB" >>$restore_file
 			fi
 			
 			if [ $COMPRESSION == "none" ] ;then
 				printf ' ... '
-				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} > $BACKUP_DIR/$db/$db-$table.sql
+				filedump="$BACKUP_DIR/$db/$db-$table.sql"
+				restorefiledump=$(basename $filedump)
+				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} > $filedump
+				echo "
+				echo \"Dumping $restorefiledump ...\"
+				cat $restorefiledump | mysql \$MYSQL_HUP --default-character-set=utf8 \$RESTOREDB" >>$restore_file
 			fi
+
 		fi
 
 		printf '\n'
@@ -165,6 +277,18 @@ for db in $databaselist; do
 		#TODO create restore script
 		
 	done
+
+	echo "
+		echo \" \"
+		echo \"Finished.\"
+		echo \" \"
+		echo \" \"
+		mysqlshow \$MYSQL_HUP \$RESTOREDB
+		echo \" \"
+		
+	" >>$restore_file
+
+	exit 0
 
 	#printf '%s\n' $BACKUP_DIR/$db
 
