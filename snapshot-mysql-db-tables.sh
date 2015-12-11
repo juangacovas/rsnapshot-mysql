b7eb9e3234960d2edbb5fec30c03faf4a48b695f
Juanga Covas <juangacovasegea@gmail.com>
2015-12-11 20:18:57 +0000

    - Added test mode and version info, tabs cleanup
    git-svn-id: https://62.117.221.7/svn/rsnapshot-mysql/trunk@4 7544ec68-d604-754e-9b34-880144e6ea8b

--- snapshot-mysql-db-tables.sh
+++ snapshot-mysql-db-tables.sh
@@ -10,20 +10,25 @@
 ############################################################################################################################################################################
 
 BACKUP_DIR="./mysqldumps"
-mkdir -p $BACKUP_DIR
 
 TIMESTAMP=$(date +"%Y-%m-%d-%H.%M")
 
+echo "-----------------------------------------------------------------------------------------"
+echo "snapshot-mysql-db-tables.sh    by Juanga Covas 2015"
+echo " "
+
 if [ -z $2 ] ;then
-	echo "Required parameters: dbhost  compression"
-	echo "            Example: my.db.server none|gz|bz2"
+	echo " "
+	echo "Required parameters: dbhost        compression   [test]"
+	echo "            Example: my.db.server  none|gz|bz2   test"
 	echo " "
 	echo "Will try to connect with user: remotebackup  password: /root/snapshot-db-pwd.txt"
-        exit 1;
+	exit 1;
 fi
 
 MYSQL_HOST=$1
 COMPRESSION=$2
+TEST_RUN=$3
 
 MYSQL_USER="remotebackup"
 MYSQL_PASSWORD_FILE="/root/snapshot-db-pwd.txt"
@@ -31,26 +36,32 @@ MYSQL_PASSWORD=`cat $MYSQL_PASSWORD_FILE`
 MYSQL_HUP="--host=$MYSQL_HOST --user=$MYSQL_USER -p$MYSQL_PASSWORD"
 MYSQL_DUMP_FLAGS="--compress --hex-blob --force --single-transaction --skip-dump-date"
 
-echo "-------------------------------------------------------------------------------"
-echo "Will try to dump databases from $MYSQL_HOST ..."
+echo "Will try to dump databases from: $MYSQL_HOST to: $BACKUP_DIR"
 echo " "
+if [ ! -z "$TEST_RUN" ]; then
+	echo "(TEST RUN) Will NOT dump anything."
+	echo " "
+fi
 
 if [ $COMPRESSION == "gz" ] ;then
 	echo "Compress to .sql.gz (gzip)"
 else
-        if [ $COMPRESSION == "bz2" ] ;then
+	if [ $COMPRESSION == "bz2" ] ;then
 		echo "Compress to .sql.bz2 (bzip2)"
-        else
-                if [ $COMPRESSION == "none" ] ;then
+	else
+		if [ $COMPRESSION == "none" ] ;then
 			echo "No compression: .sql"
-                else
-                        echo "ERROR: valid compression parameter is none|gz|bz2"
-                        exit 1;
-                fi
-        fi
+		else
+			echo "ERROR: valid compression parameter is none|gz|bz2"
+			exit 1;
+		fi
+	fi
 fi
 
-printf 'MySQL connection test ... '
+mkdir -p $BACKUP_DIR
+
+echo " "
+printf 'Testing connection to MySQL ... '
 
 RESULT=`mysqlshow $MYSQL_HUP | grep -v Wildcard | grep -o Databases`
 if [ "$RESULT" == "Databases" ]; then
@@ -60,76 +71,106 @@ else
 	exit 1;
 fi
 
-echo "Getting database list ..."
+echo " "
+echo "MySQL version info"
+if [ -z "$TEST_RUN" ]; then
+	mysql $MYSQL_HUP --skip-column-names -e"SHOW VARIABLES LIKE '%version%';" > $BACKUPDIR/mysql-version-$MYSQL_HOST.txt
+fi
+mysql $MYSQL_HUP --skip-column-names -e"SHOW VARIABLES LIKE '%version%';"
+echo " "
+
+if [ -z "$TEST_RUN" ]; then
+	echo "Dumping GRANTs to $BACKUP_DIR/mysql-grants-$MYSQL_HOST.sql"
+	mysql $MYSQL_HUP --no-auto-rehash --skip-column-names -e"SELECT CONCAT('SHOW GRANTS FOR ''',user,'''@''',host,''';') FROM mysql.user WHERE user<>''" | mysql --host=$MYSQL_HOST --user=$MYSQL_USER -p$MYSQL_PASSWORD --no-auto-rehash --skip-column-names | sed 's/$/;/g' > $BACKUP_DIR/mysql-grants-$MYSQL_HOST.sql
+else
+	echo "(TEST RUN) Skipping GRANTs dump"
+fi
+echo " "
+
+echo "Getting database list to dump ... "
 
 databaselist=`mysql $MYSQL_HUP --no-auto-rehash -e "SHOW DATABASES;" | grep -Ev "(^mysql$|^phpmyadmin$|^Database$|information_schema|performance_schema|^test_|_test$|_bak$|^bak_)"`
 
 # loop all database names
 for db in $databaselist; do
 
-        # exclude system databases
-        if [ $db != "mysql" ] && [ $db != "phpmyadmin" ] ;then
+	# exclude system databases
+	if [ $db == "mysql" ] || [ $db == "phpmyadmin" ] ;then
+		continue
+	fi
 
-                printf 'Dumping DB: %s \n' $db
+	if [ ! -z "$TEST_RUN" ]; then
+		printf 'Found DB: %s \n' $db
+	else
+		printf 'Dumping DB: %s \n' $db
+	fi
 
-		# get a list of db.table.engine
-                db_table_engine_list=`mysql $MYSQL_HUP --no-auto-rehash --skip-column-names -e "SELECT CONCAT(table_schema,'.',table_name,'.',engine) FROM information_schema.tables WHERE table_schema = '${db}'"`
-                db_table_list=`mysql $MYSQL_HUP --no-auto-rehash --skip-column-names -e "SELECT CONCAT(table_schema,'.',table_name) FROM information_schema.tables WHERE table_schema = '${db}'"`
+	# get a list of db.table.engine
+	db_table_engine_list=`mysql $MYSQL_HUP --no-auto-rehash --skip-column-names -e "SELECT CONCAT(table_schema,'.',table_name,'.',engine) FROM information_schema.tables WHERE table_schema = '${db}'"`
+	db_table_list=`mysql $MYSQL_HUP --no-auto-rehash --skip-column-names -e "SELECT CONCAT(table_schema,'.',table_name) FROM information_schema.tables WHERE table_schema = '${db}'"`
 
-                mkdir -p $BACKUP_DIR/$db
-                echo $db_table_list > $BACKUP_DIR/$db/$db-tablelist.txt
+	mkdir -p $BACKUP_DIR/$db
+	if [ -z "$TEST_RUN" ]; then
+		echo $db_table_list > $BACKUP_DIR/$db/$db-tablelist.txt
+	fi
 
-                for DBTBNG in $db_table_engine_list; do
-                        #db=`echo ${DBTBNG} | sed 's/\./ /g' | awk '{print $1}'`
-                        table=`echo ${DBTBNG} | sed 's/\./ /g' | awk '{print $2}'`
-                        engine=`echo ${DBTBNG} | sed 's/\./ /g' | awk '{print $3}'`
+	for DBTBNG in $db_table_engine_list; do
 
-                        printf '        Dumping %s table: %s.sql' $engine $table
+		#db=`echo ${DBTBNG} | sed 's/\./ /g' | awk '{print $1}'`
+		table=`echo ${DBTBNG} | sed 's/\./ /g' | awk '{print $2}'`
+		engine=`echo ${DBTBNG} | sed 's/\./ /g' | awk '{print $3}'`
 
-			# --skip-dump-date so dump data do NOT differ if table did not really change
-                      	# --single-transaction for properly dumping InnoDB table. This automatically turns off --lock-tables (needed for MyISAM dump)
-                      	# --lock-tables for properly dumping MyISAM table, which anyway is enabled by default
-                      	# --force  Continue even if we get an SQL error.
+		if [ -z "$TEST_RUN" ]; then
+			printf '        Dumping %s table as: %s.sql' $engine $table
+		else
+			printf '          %s table: %s' $engine $table
+		fi
 
-			# Table Dump includes DROP TABLE IF EXISTS
+		# --skip-dump-date so dump data do NOT differ if table did not really change
+		# --single-transaction for properly dumping InnoDB table. This automatically turns off --lock-tables (needed for MyISAM dump)
+		# --lock-tables for properly dumping MyISAM table, which anyway is enabled by default
+		# --force  Continue even if we get an SQL error.
 
-                        if [ $engine == "InnoDB" ] ;then
-				ENGINE_OPT="--single-transaction"
-                        else
-	                	if [ $engine == "MyISAM" ] ;then
-					ENGINE_OPT="--lock-tables"
-	                        else
-	                        	printf "ERROR: Unexpected table engine: [$engine].\n"
-	                        fi
-                        fi
+		# Table Dump includes DROP TABLE IF EXISTS
 
+		if [ $engine == "InnoDB" ] ;then
+			ENGINE_OPT="--single-transaction"
+		else
+			if [ $engine == "MyISAM" ] ;then
+				ENGINE_OPT="--lock-tables"
+			else
+				printf "ERROR: Unexpected table engine: [$engine].\n"
+			fi
+		fi
+
+		if [ -z "$TEST_RUN" ]; then
 			if [ $COMPRESSION == "gz" ] ;then
 				printf '.gz ... '
-	 			mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | gzip -9 > $BACKUP_DIR/$db/$db-$table.sql.gz
+				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | gzip -9 > $BACKUP_DIR/$db/$db-$table.sql.gz
 			fi
-                        if [ $COMPRESSION == "bz2" ] ;then
+			
+			if [ $COMPRESSION == "bz2" ] ;then
 				printf '.bz2 ... '
-	                        mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | bzip2 -cq9 > $BACKUP_DIR/$db/$db-$table.sql.bz2
+				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} | bzip2 -cq9 > $BACKUP_DIR/$db/$db-$table.sql.bz2
 			fi
-                        if [ $COMPRESSION == "none" ] ;then
+			
+			if [ $COMPRESSION == "none" ] ;then
 				printf ' ... '
-                                mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} > $BACKUP_DIR/$db/$db-$table.sql
+				mysqldump $MYSQL_HUP $MYSQL_DUMP_FLAGS $ENGINE_OPT ${db} ${table} > $BACKUP_DIR/$db/$db-$table.sql
 			fi
+		fi
 
-			printf '\n'
+		printf '\n'
 
-                        # TODO create restore script
-                done
+		#TODO create restore script
+		
+	done
 
-                #printf '%s\n' $BACKUP_DIR/$db
+	#printf '%s\n' $BACKUP_DIR/$db
 
-        fi
 done
 
 echo " "
-echo "Dumping grants to $BACKUP_DIR/mysqlGrants-$MYSQL_HOST.sql"
-mysql --host=$MYSQL_HOST --user=$MYSQL_USER -p$MYSQL_PASSWORD --no-auto-rehash --skip-column-names -e"SELECT CONCAT('SHOW GRANTS FOR ''',user,'''@''',host,''';') FROM mysql.user WHERE user<>''" | mysql --host=$MYSQL_HOST --user=$MYSQL_USER -p$MYSQL_PASSWORD --no-auto-rehash --skip-column-names | sed 's/$/;/g' > $BACKUP_DIR/mysqlGrants-$MYSQL_HOST.sql
-
 echo "Finished."
 
 exit 0
